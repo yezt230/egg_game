bac7fd090d0cc12ba82efee2c416991992ec5b4a
yezt230 <mjollnirsandwich@gmail.com>
2025-04-05 14:22:00 -0500

    Able to change position in the middle of swallowing animation

--- scenes/swallow_down.gd
+++ scenes/swallow_down.gd
@@ -1,9 +1,20 @@
 extends State
 
 @export var poised_down_state: State
+@export var poised_up_state: State
+
+var collision
+var player_sprite
+var sprite_scale
+var collision_coords
 
 func enter() -> void:
 	super()
+	collision = parent.collision
+	player_sprite = parent.player_sprite
+	sprite_scale = parent.sprite_scale
+	collision_coords = parent.collision_coords
+	
 	if not parent.player_animations.is_connected("animation_finished", Callable(self, "_on_animation_player_animation_finished")):
 		parent.player_animations.connect("animation_finished", Callable(self, "_on_animation_player_animation_finished"))
 	parent.player_animations.play('swallow_down')
@@ -13,6 +24,31 @@ func exit():
 	if parent.player_animations.is_connected("animation_finished", Callable(self, "_on_animation_player_animation_finished")):
 		parent.player_animations.disconnect("animation_finished", Callable(self, "_on_animation_player_animation_finished"))
 
+
+func process_input(_event: InputEvent) -> State:
+	if Input.is_action_just_pressed('up'):
+		#idle_timer.restart()
+		return poised_up_state
+	if Input.is_action_just_pressed('left'):
+		print("left")
+		collision.global_position.x = collision_coords.right
+		#idle_timer.restart()
+		player_sprite.scale.x = sprite_scale * 1
+	elif Input.is_action_just_pressed('right'):
+		print("right")
+		collision.global_position.x = collision_coords.left
+		player_sprite.scale.x = sprite_scale * -1
+	elif Input.is_action_just_pressed('diagonal'):
+		if collision.global_position.x == collision_coords.right:
+			print("collision was left")
+			collision.global_position.x = collision_coords.left
+		elif collision.global_position.x == collision_coords.left:
+			print("collision was right")
+			collision.global_position.x = collision_coords.right
+		player_sprite.scale.x = player_sprite.scale.x * -1
+		return poised_up_state
+	return null
+	
 
 func on_enemy_eaten():
 	pass
--- scenes/swallow_up.gd
+++ scenes/swallow_up.gd
@@ -1,6 +1,7 @@
 extends State
 
 @export var poised_up_state: State
+@export var poised_down_state: State
 @export var stifled_state: State
 
 #initializing and keeping track of burp_counter means it can only increment in the swallow_up state,
@@ -9,9 +10,19 @@ extends State
 @onready var burp_counter = 0
 @onready var burp_limit = 5
 
+var collision
+var player_sprite
+var sprite_scale
+var collision_coords
+
 func enter() -> void:
 	super()
 	burp_counter += 1
+	collision = parent.collision
+	player_sprite = parent.player_sprite
+	sprite_scale = parent.sprite_scale
+	collision_coords = parent.collision_coords
+	
 	if not parent.player_animations.is_connected("animation_finished", Callable(self, "_on_animation_player_animation_finished")):
 		parent.player_animations.connect("animation_finished", Callable(self, "_on_animation_player_animation_finished"))
 	parent.player_animations.play('swallow_up')
@@ -21,6 +32,28 @@ func exit() -> void:
 	if parent.player_animations.is_connected("animation_finished", Callable(self, "_on_animation_player_animation_finished")):
 		parent.player_animations.disconnect("animation_finished", Callable(self, "_on_animation_player_animation_finished"))
 
+
+func process_input(_event: InputEvent) -> State:
+	if Input.is_action_just_pressed('down'):		
+		return poised_down_state
+	if Input.is_action_just_pressed('left'):
+		collision.global_position.x = collision_coords.right
+		player_sprite.scale.x = sprite_scale * 1
+	elif Input.is_action_just_pressed('right'):
+		collision.global_position.x = collision_coords.left
+		player_sprite.scale.x = sprite_scale * -1
+	elif Input.is_action_just_pressed('diagonal'):
+		if collision.global_position.x == collision_coords.right:
+			print("collision was left")
+			collision.global_position.x = collision_coords.left
+		elif collision.global_position.x == collision_coords.left:
+			print("collision was right")
+			collision.global_position.x = collision_coords.right
+			print(str(collision.global_position.x))
+		player_sprite.scale.x = player_sprite.scale.x * -1
+		return poised_down_state
+	return null
+	
 
 func on_enemy_eaten():
 	pass
