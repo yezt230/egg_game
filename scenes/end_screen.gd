2ee6fc6aec4537fb859b0e826f77485c57109e7d
yezt230 <mjollnirsandwich@gmail.com>
2025-05-24 12:34:00 -0500

    Functional end screen buttons (Restart, Main Menu, Quit)

--- scenes/end_screen.gd
+++ scenes/end_screen.gd
@@ -10,3 +10,12 @@ func set_defeat(score_string):
 func _on_restart_button_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_main_menu_button_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
