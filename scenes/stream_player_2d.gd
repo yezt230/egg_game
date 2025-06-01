adf844bf1925e70ace87d995ad5af9987a7720a2
yezt230 <mjollnirsandwich@gmail.com>
2025-06-01 11:28:26 -0500

    Randomized different belches

--- scenes/stream_player_2d.gd
+++ scenes/stream_player_2d.gd
@@ -1,6 +1,12 @@
 extends AudioStreamPlayer2D
 
+@export var SoundArray: Array[AudioStream]
 
 func play_belch():
+	if SoundArray == null || SoundArray.size() == 0:
+		return
+	
+	stream = SoundArray.pick_random()
 	play()
+
 	
