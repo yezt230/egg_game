7ddbe8b25864be15e07e45dcef6c20f528149a4d
yezt230 <mjollnirsandwich@gmail.com>
2025-06-01 12:27:37 -0500

    Basic state machine for main menu, to handle Main/Settings/Credits screens

--- scenes/start_screen.gd
+++ scenes/start_screen.gd
@@ -1,7 +1,28 @@
 extends CanvasLayer
 
-func _ready():
+@onready var main_buttons = [$MarginContainer/PanelContainer/MarginContainer/VBoxContainer/StartButton,
+$MarginContainer/PanelContainer/MarginContainer/VBoxContainer/SettingsButton,
+$MarginContainer/PanelContainer/MarginContainer/VBoxContainer/CreditsButton]
+@onready var title_label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/TitleLabel
+
+var states = ["main", "settings", "credits"]
+var state = states[0]
+
+func _ready():	
 	pass
+	#main_buttons[1].visible = false
+	
 	
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_settings_button_pressed():
+	state = states[1]
+	title_label.text = "Settings"
+	hide_main_buttons()
+	
+	
+func hide_main_buttons():
+	for main_button in main_buttons:
+		main_button.visible = false
