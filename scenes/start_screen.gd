ff1742c39d3c921c52c4e23084440542bfb15be3
yezt230 <mjollnirsandwich@gmail.com>
2025-06-01 13:04:16 -0500

    Minor changes to title screen

--- scenes/start_screen.gd
+++ scenes/start_screen.gd
@@ -6,13 +6,12 @@ $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/CreditsButton]
 @onready var title_label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/TitleLabel
 @onready var main_menu_button = %MainButton
 
-var title = "Bear Game"
+var game_title = "Bear Game"
 var states = ["main", "settings", "credits"]
 var state = states[0]
 
 func _ready():	
 	main_menu_button.visible = false
-	#main_buttons[1].visible = false
 	
 	
 func _on_start_button_pressed():
@@ -23,6 +22,10 @@ func _on_settings_button_pressed():
 	change_display(1, "Settings")
 	
 	
+func _on_credits_button_pressed():
+	change_display(2, "Credits")
+	
+
 func hide_main_buttons():
 	for main_button in main_buttons:
 		main_button.visible = false
@@ -37,7 +40,7 @@ func show_main_buttons():
 
 func return_to_main():
 	state = states[0]
-	title_label.text = title
+	title_label.text = game_title
 	show_main_buttons()
 
 
@@ -49,7 +52,3 @@ func change_display(state_chosen: int, title_text: String):
 
 func _on_main_button_pressed():
 	return_to_main()	
-
-
-func _on_credits_button_pressed():
-	change_display(2, "Credits")
