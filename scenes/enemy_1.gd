047b44756619044b557d627eb15f18e9c8e161d4
yezt230 <mjollnirsandwich@gmail.com>
2025-06-07 11:51:20 -0500

    Checks whether critter is on branch or falling and changes sprite accordingly

--- scenes/enemy_1.gd
+++ scenes/enemy_1.gd
@@ -13,6 +13,7 @@ signal enemy_escaped
 @onready var spawn_bottom_left = get_node("/root/Main/Platforms/MarkerBottomLeft")
 @onready var spawn_bottom_right = get_node("/root/Main/Platforms/MarkerBottomRight")
 #@onready var score_scene = get_node("/root/Main/Score")
+@onready var falling_label = $FallingLabel
 
 const GRAVITY = 25000
 const FLOOR_NORMAL = Vector2.UP
@@ -27,7 +28,6 @@ var has_been_eaten = false
 var has_escaped = false
 var move_speed = 0
 var regex = RegEx.new()
-var score_array = [2,4,6,8]
 #var will_generate_belch_initiator = false
 
 func _ready():
@@ -48,35 +48,35 @@ func _ready():
 	
 	enemy_animations.play("sliding")
 	var animal = randi() % 3
-	match animal:
-		0:
-			enemy_sprite.frame = animals["rabbit"]
-			animal_type = 0
-		1:
-			enemy_sprite.frame = animals["raccoon"]
-			animal_type = 1
-		2:
-			enemy_sprite.frame = animals["beaver"]
-			animal_type = 2
+	set_animal(animal)
 			
 
 func _physics_process(delta):		
 	#falling_speed is really the overall movement speed
 	var falling_speed = GRAVITY * delta
 	move_and_slide()
+	if get_slide_collision_count() > 0:
+		falling_label.text = "on branch"
+		set_animal(1)
+		enemy_animations.play("sliding")
+	else:
+		falling_label.text = "falling"
+		enemy_animations.play("falling")
 	for i in range(get_slide_collision_count()):
-		var collision = get_slide_collision(i)
+		
+		#print(str(i) + str(get_slide_collision(i).get_collider().name))
+		is_falling(get_slide_collision(i), delta, falling_speed)
+			#falling_label.text = "falling"
+		#else:
+			#falling_label.text = "on branch"
 
-		var collider_name = collision.get_collider().name
-		if collider_name == "Player" and not has_been_eaten:
-			has_been_eaten = true
-			emit_signal("enemy_eaten")
-			queue_free()
-			
-		if regex.search(collider_name):
-			falling_speed = move_speed * delta
-		else:
-			falling_speed = GRAVITY * delta
+		#if not is_falling():
+		##if regex.search(collider_name):
+			#falling_speed = move_speed * delta
+		#else:
+			#print("dragonhead")
+			#falling_speed = GRAVITY * delta
+			#enemy_animations.play("falling")
 			
 	if self.global_position.y > 550:		
 		if not has_escaped:
@@ -99,6 +99,11 @@ func _physics_process(delta):
 	else:
 		velocity.y = falling_speed
 	
+	#if is_falling():
+		#falling_label.text = "falling"
+	#else:
+		#falling_label.text = "on branch"
+	#
 
 func _on_lifespan_timer_timeout():
 	queue_free()
@@ -115,8 +120,37 @@ func determine_spot():
 	self.scale.x = enemy_scale
 
 
-func set_animal():
-	pass
+func set_animal(animal):
+	match animal:
+		0:
+			enemy_sprite.frame = animals["rabbit"]
+			animal_type = 0
+		1:
+			enemy_sprite.frame = animals["raccoon"]
+			animal_type = 1
+		2:
+			enemy_sprite.frame = animals["beaver"]
+			animal_type = 2
+
+
+func is_falling(collider, delta, physics_falling_speed):
+	var collision = collider
+
+	var collider_name = collision.get_collider().name
+	#print(collider_name)
+	if collider_name == "Player" and not has_been_eaten:
+		has_been_eaten = true
+		emit_signal("enemy_eaten")
+		queue_free()			
+	print(regex.search(collider_name))
+	if regex.search(collider_name):
+		physics_falling_speed = move_speed * delta
+		print(str(regex.search(collider_name)))
+		#return false
+	else:
+		physics_falling_speed = GRAVITY * delta
+		#print("002")
+		print("not touching branch?")
 
 
 func set_speed_increase(speed_increase):
